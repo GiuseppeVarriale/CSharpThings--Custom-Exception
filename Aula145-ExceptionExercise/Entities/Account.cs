77afdcb2f7e739c46c18c60464bb8df8318425b4
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-22 14:48:47 -0300

    Create Account Entity

---
+++ Aula145-ExceptionExercise/Entities/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula145_ExceptionExercise.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; private set; }
+        public double WithdrawLimit { get; private set; }
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine("not implemented yet");
+        }
+        public void withdraw(double amount) 
+        {
+            Console.WriteLine("not implemented yet");
+        }
+    }
+}
