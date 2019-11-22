908506f874b308c2710e7fcc39b3a67a37413cfd
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-22 15:32:45 -0300

    Add logic to Program.cs (main)

--- Aula145-ExceptionExercise/Program.cs
+++ Aula145-ExceptionExercise/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using Aula145_ExceptionExercise.Entities;
+using Aula145_ExceptionExercise.Entities.Exceptions;
 
 namespace Aula145_ExceptionExercise
 {
@@ -6,7 +9,40 @@ namespace Aula145_ExceptionExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial Balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Account account = new Account(number, holder, balance, withdrawLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account.Withdraw(amount);
+                Console.WriteLine($"New balance: {account.Balance.ToString("F2",CultureInfo.InvariantCulture)}");
+                   
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+
         }
     }
 }
