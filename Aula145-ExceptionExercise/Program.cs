76ff5bebff3ac2cc47a761077cdd52421e4d772d
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-22 14:32:33 -0300

    Initial Commit

---
+++ Aula145-ExceptionExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula145_ExceptionExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
